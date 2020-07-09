ed6ead4b4638ba62b483cfc58a1908e8d08075e9
cardoso <matheus@cardo.so>
2020-07-08 22:15:39 -0300

    Init Stream Chat

--- LiveFitnessApp/LiveFitnessApp/AppDelegate.swift
+++ LiveFitnessApp/LiveFitnessApp/AppDelegate.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import StreamChatClient
+import VoxeetSDK
+import VoxeetUXKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Client.configureShared(.init(apiKey: "74e5enp33qj2", logOptions: .info))
+        
+        VoxeetSDK.shared.initialize(consumerKey: "ZTBib3I3NzkzMmt0aA==", consumerSecret: "NDUyM2kzMTc0ZHNvZWxjaHRucG41dmpidnE=")
+        VoxeetUXKit.shared.initialize()
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -34,4 +38,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
